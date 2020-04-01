9201384120568b71414fb9bef1d20a1c0dc3d955
ramezanikebrya <hamid.ramezani1375@gmail.com>
2020-04-01 19:51:45 +0200

    Revert "add generated files to gitignore"
    This reverts commit c9cbb7e492e93f15e9a179f74a5c37c0dcaeef31.

--- Debug/src/Main.d
+++ Debug/src/Main.d
@@ -1,10 +1,8 @@
 src/Main.d: ../src/Main.cpp ../src/Person.h ../src/Enumerated_Types.h \
- ../src/Utils.h ../src/Random_Element.h
+ ../src/Utils.h
 
 ../src/Person.h:
 
 ../src/Enumerated_Types.h:
 
 ../src/Utils.h:
-
-../src/Random_Element.h:
